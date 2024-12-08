4ad5328612b8e72050d921200e0fa0a3d37761e8
fravaderu <fravaderu@gmail.com>
2024-12-08 11:08:47 +0700

    add file

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin')
+        ]);
+
+        $admin->assignRole('admin');
+
+        $pengguna = User::create([
+            'name' => 'pengguna',
+            'username' => 'pengguna',
+            'email' => 'pengguna@gmail.com',
+            'password' => bcrypt('pengguna')
+        ]);
+
+        $pengguna->assignRole('pengguna');
+    }
+}
