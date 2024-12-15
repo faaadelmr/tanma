0daa2b35d5b1ea8eca3c08524f9d55c0d879a981
fravadearu <fravadearu@gmail.com>
2024-12-15 08:11:05 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserManagementController;
-use App\Http\Controllers\ReportController;
-use App\Http\Controllers\TaskCategoryController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,15 +23,8 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::resource('users', UserManagementController::class);
 
 });
-Route::middleware(['auth'])->group(function () {
-    Route::resource('reports', ReportController::class);
-});
+
 
 
 require __DIR__.'/auth.php';
 
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('task-categories', TaskCategoryController::class);
-});
