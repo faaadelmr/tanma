39600f83ab9d62d318b809a3e001f2fd3175c587
kitacobatest <kitacobatest@gmail.com>
2024-12-19 10:24:47 +0000

    update navigasi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -52,10 +52,11 @@
         </ul>
     </div>
     <div class="navbar-end">
+        <div><p>Hi! {{auth()->user()->name}}</p></div>
         <div class="dropdown dropdown-end">
             <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
                 <div class="w-10 rounded-full">
-                    <img alt="Null Foto Profil" src="" />
+                    <img alt="Null Foto Profil" src="{{ auth()->user()->path ? asset('storage/' . auth()->user()->path) : asset('images/default-avatar.png') }}" />
                 </div>
             </div>
             <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
