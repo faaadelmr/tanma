8e4bb51cc11a74c34b34376bc8d35573ba1f083f
fravadearu <fravadearu@gmail.com>
2025-01-02 06:07:27 +0000

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }}</title>
+    <title>Tanma</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -37,7 +37,7 @@
                 </div>
             </header>
         @endisset
-        
+
 
         <!-- Page Content -->
         <main data-theme="" class="border-accent rounded-md">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('tanma.png') }}" type="image/png"/>
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Tanma</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
